036edfed94ff4bf6f75f1740fe1bdef50ffadede
alexhutko <ahutko@gmail.com>
2020-12-19 01:03:12 -0800

    update

--- make_histograms.py
+++ make_histograms.py
@@ -53,149 +53,7 @@ for n in [ 0, 1 , 2, 3 ]:
         staltas[n].append(min(stalta,75))
         deps[n].append(dep)
 
-
-#----- histogram of distances
-nbins = 18
-x = distances
-plt.rcParams['font.size'] = '6'
-fig, axs = plt.subplots(2, 2)
-axs[0,0].set_title('M4-5 West Coast')
-axs[0,0].hist(x[0],nbins,label=('n='+str(len(x[0]))))
-axs[0,0].legend(prop={'size': 8})
-
-axs[0,1].set_title('North Am. M>5')
-axs[0,1].hist(x[1],nbins,label=('n='+str(len(x[1]))))
-axs[0,1].legend(prop={'size': 8})
-
-axs[1,0].set_title('Global z<100 km M>=6')
-axs[1,0].hist(x[2],nbins,label=('n='+str(len(x[2]))))
-axs[1,0].legend(prop={'size': 8})
-
-axs[1,1].set_title('Global z>100 km M>=6')
-axs[1,1].hist(x[3],nbins,label=('n='+str(len(x[3]))))
-axs[1,1].legend(prop={'size': 8})
-fig.suptitle('Epicentral distance',fontsize=12)
-figname = 'Distances_histogram.png'
-plt.savefig(figname, dpi=180)
-
-#----- histogram of magnitudes
-x = mags
-fig, axs = plt.subplots(2, 2)
-axs[0,0].hist(x[0],int(10*(max(x[0]) - min(x[0]))),label=('n='+str(len(x[0]))))
-axs[0,0].set_title('M4-5 West Coast')
-axs[0,0].legend(prop={'size': 8})
-
-axs[0,1].hist(x[1],int(10*(max(x[1]) - min(x[1]))),label=('n='+str(len(x[1]))))
-axs[0,1].set_title('North Am. M>5')
-axs[0,1].legend(prop={'size': 8})
-
-axs[1,0].hist(x[2],int(10*(max(x[2]) - min(x[2]))),label=('n='+str(len(x[2]))))
-axs[1,0].set_title('Global z<100 km M>=6')
-axs[1,0].legend(prop={'size': 8})
-
-axs[1,1].hist(x[3],int(10*(max(x[3]) - min(x[3]))),label=('n='+str(len(x[3]))))
-axs[1,1].set_title('Global z>100 km M>=6')
-axs[1,1].legend(prop={'size': 8})
-fig.suptitle('Magnitudes',fontsize=12)
-figname = 'Mags_histogram.png'
-plt.savefig(figname, dpi=180)
-
-#----- histogram of STA/LTA ratios
-nbins = 40
-x = staltas
-fig, axs = plt.subplots(2, 2)
-axs[0,0].hist(x[0],nbins,label=('n='+str(len(x[0]))))
-axs[0,0].set_title('M4-5 West Coast')
-axs[0,0].legend(prop={'size': 8})
-
-axs[0,1].hist(x[1],nbins,label=('n='+str(len(x[1]))))
-axs[0,1].set_title('North Am. M>5')
-axs[0,1].legend(prop={'size': 8})
-
-axs[1,0].hist(x[2],nbins,label=('n='+str(len(x[2]))))
-axs[1,0].set_title('Global z<100 km M>=6')
-axs[1,0].legend(prop={'size': 8})
-
-axs[1,1].hist(x[3],nbins,label=('n='+str(len(x[3]))))
-axs[1,1].set_title('Global z>100 km M>=6')
-axs[1,1].legend(prop={'size': 8})
-fig.suptitle('STA/LTA ratio',fontsize=12)
-figname = 'Staltas_histogram.png'
-plt.savefig(figname, dpi=180)
-
-#--------- now just those that have a YES trigger 
-
-#----- histogram of distances
-nbins = 18
-x = distancesY
-plt.rcParams['font.size'] = '6'
-fig, axs = plt.subplots(2, 2)
-axs[0,0].set_title('M4-5 West Coast')
-axs[0,0].hist(x[0],nbins,label=('n='+str(len(x[0]))))
-axs[0,0].legend(prop={'size': 8})
-
-axs[0,1].set_title('North Am. M>5')
-axs[0,1].hist(x[1],nbins,label=('n='+str(len(x[1]))))
-axs[0,1].legend(prop={'size': 8})
-
-axs[1,0].set_title('Global z<100 km M>=6')
-axs[1,0].hist(x[2],nbins,label=('n='+str(len(x[2]))))
-axs[1,0].legend(prop={'size': 8})
-
-axs[1,1].set_title('Global z>100 km M>=6')
-axs[1,1].hist(x[3],nbins,label=('n='+str(len(x[3]))))
-axs[1,1].legend(prop={'size': 8})
-fig.suptitle('Epicentral distance, only those with Trigger=YES',fontsize=12)
-figname = 'Distances_histogram_Trigs_only.png'
-plt.savefig(figname, dpi=180)
-
-#----- histogram of magnitudes
-x = magsY
-fig, axs = plt.subplots(2, 2)
-axs[0,0].hist(x[0],int(10*(max(x[0]) - min(x[0]))),label=('n='+str(len(x[0]))))
-axs[0,0].set_title('M4-5 West Coast')
-axs[0,0].legend(prop={'size': 8})
-
-axs[0,1].hist(x[1],int(10*(max(x[1]) - min(x[1]))),label=('n='+str(len(x[1]))))
-axs[0,1].set_title('North Am. M>5')
-axs[0,1].legend(prop={'size': 8})
-
-axs[1,0].hist(x[2],int(10*(max(x[2]) - min(x[2]))),label=('n='+str(len(x[2]))))
-axs[1,0].set_title('Global z<100 km M>=6')
-axs[1,0].legend(prop={'size': 8})
-
-axs[1,1].hist(x[3],int(10*(max(x[3]) - min(x[3]))),label=('n='+str(len(x[3]))))
-axs[1,1].set_title('Global z>100 km M>=6')
-axs[1,1].legend(prop={'size': 8})
-fig.suptitle('Magnitudes, only those with Trigger=YES',fontsize=12)
-figname = 'Mags_histogram_Trigs_only.png'
-plt.savefig(figname, dpi=180)
-
-#----- histogram of STA/LTA ratios
-nbins = 40
-x = staltasY
-fig, axs = plt.subplots(2, 2)
-axs[0,0].hist(x[0],nbins,label=('n='+str(len(x[0]))))
-axs[0,0].set_title('M4-5 West Coast')
-axs[0,0].legend(prop={'size': 8})
-
-axs[0,1].hist(x[1],nbins,label=('n='+str(len(x[1]))))
-axs[0,1].set_title('North Am. M>5')
-axs[0,1].legend(prop={'size': 8})
-
-axs[1,0].hist(x[2],nbins,label=('n='+str(len(x[2]))))
-axs[1,0].set_title('Global z<100 km M>=6')
-axs[1,0].legend(prop={'size': 8})
-
-axs[1,1].hist(x[3],nbins,label=('n='+str(len(x[3]))))
-axs[1,1].set_title('Global z>100 km M>=6')
-axs[1,1].legend(prop={'size': 8})
-fig.suptitle('STA/LTA ratio, only those with Trigger=YES',fontsize=12)
-figname = 'Staltas_histogram_Trigs_only.png'
-plt.savefig(figname, dpi=180)
-
-
-#--------- Try stacked histograms
+#--------- Plot stacked histograms
 
 #----- histogram of distances
 nbins = 18
@@ -225,10 +83,12 @@ axs[1,0].set_title('Global z<100 km M>=6')
 axs[1,0].hist([xY[2],xN[2]],nbins,stacked=True,label=('nTrig='+str(len(xY[2])),'nTotal='+str(len(x[2]))))
 axs[1,0].legend(prop={'size': 8})
 axs[1,0].set_ylabel('N P-wavelets')
+axs[1,0].set_xlabel('Degrees')
 
 axs[1,1].set_title('Global z>100 km M>=6')
 axs[1,1].hist([xY[3],xN[3]],nbins,stacked=True,label=('nTrig='+str(len(xY[3])),'nTotal='+str(len(x[3]))))
 axs[1,1].legend(prop={'size': 8})
+axs[1,1].set_xlabel('Degrees')
 fig.suptitle('Epicentral distances',fontsize=12)
 figname = 'Distances_histogram.png'
 plt.savefig(figname, dpi=180)
